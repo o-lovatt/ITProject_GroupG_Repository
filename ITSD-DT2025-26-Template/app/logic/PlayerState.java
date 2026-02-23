54c3298bc1a52f673a88cdc57175b2b261cc8796
“Oliver <oliver.joe.lovatt@gmail.com>
2026-02-23 18:08:20 +0000

    Initial commit

---
+++ ITSD-DT2025-26-Template/app/logic/PlayerState.java
@@ -0,0 +1,74 @@
+//package logic;
+//
+//public class PlayerState {
+//    public static final int INITIAL_HEALTH = 20;
+//    public static final int INITIAL_MANA = 0;
+//    public static final int MAX_MANA = 9;
+//
+//    private final PlayerSide side;
+//    private final AvatarUnit avatar;
+//    private int mana;
+//    private final Deck deck;
+//    private final Hand hand;
+//
+//    public PlayerState(PlayerSide side, AvatarUnit avatar, Deck deck) {
+//        this.side = side;
+//        this.avatar = avatar;
+//        this.mana = INITIAL_MANA;
+//        this.deck = deck;
+//        this.hand = new Hand();
+//        this.avatar = setOwnersate(this);//so avatar can reference its owner
+//
+//
+//
+//        //health stuff
+//        public int getHealth() {
+//            return avatar.getCurrentHealth();//returns avatars health
+//        }
+//
+//        public void takeDamage(int damage){
+//            avatar.takeDamage(damage);//damages avatar
+//        }
+//
+//        public void heal(int amount) {
+//            avatar.heal(amount);//heals avatar
+//        }
+//
+//        public boolean isDefeated() {
+//            return avatar.isDead();//true if health = 0
+//        }
+//
+//
+//        //mana stuff
+//        public int getMana() {
+//            return mana;//return current mana
+//        }
+//
+//        public void setManaForTurn(int turnNumber){
+//            this.mana = Math.min(turnNumber + 1, MAX_MANA); //set mana to turnNumber + 1 (max mana = 9)
+//        }
+//
+//        public void useMana(int cost) {
+//            if (cost > mana) {
+//                print(side + "has insufficient mana." + cost + "mana required.");//change to error exception!!
+//            }
+//            this.mana -= cost;
+//        }
+//
+//        public boolean hasMana(int cost) {
+//            return mana >= cost; //checks if player has enough mana first, return true if they do
+//        }
+//
+//        public void resetMana() {
+//            this.mana = 0;//reset mana at END_TURN
+//        }
+//
+//        //card deck stuff
+//
+//
+//
+//
+//
+//
+//    }
+//}
---
+++ ITSD-DT2025-26-Template/app/structures/GameState.java
@@ -0,0 +1,82 @@
+package structures;
+import java.util.List;
+/**
+ * This class can be used to hold information about the on-going game.
+ * It's created with the GameActor.
+ * 
+ * @author Dr. Richard McCreadie
+ *
+ */
+public class GameState {
+    public boolean gameInitalised = false;
+
+    public boolean something = false;
+}
+
+//
+//    private final Board board;
+//    private final PlayerState humanState;
+//    private final PlayerState aiState;
+//    private PlayerSide activePlayer; //current players turn
+//    private int turnNumber;
+//    private GamePhase currentPhase; //START_TURN, MAIN_GAME, END_TURN
+//    private PlayerSide winner;// null until gameOver = true
+//    private boolean gameOver;
+//
+//
+//        public GameState(Board board, PlayerState humanState, PlayerState aiState){
+//            this.board = board;
+//            this.humanState = humanState;
+//            this.aiState = aiState;
+//            this.activePlayer = PlayerSide.HUMAN_LEFT;
+//            this.turnNumber = 1; //or 0 ??
+//            this.currentPhase = GamePhase.START_TURN;
+//            this.gameOver = false;
+//            this.winner = null;
+//        }
+//
+//
+//        public Board getBoard() {
+//            return board; //return game board
+//        }
+//
+//        public Unit getUnit(Position pos){
+//            return board.getUnit(pos); //return unit at it's position
+//        }
+//
+//        public boolean isTileEmpty(Position pos){
+//            return board.isTileEmpty(pos); //true if tile is empty
+//        }
+//
+//        public Boolean isInBounds(Position pos){
+//            return boan.isInBounds(pos); //true if pos is in 9x5 board
+//        }
+//
+//        public List<Unit> getUnitsFor(PlayerSide side){
+//            return board.getUnitsOwnedBy(side);//return all units on players side
+//        }
+//
+//        public List<Unit> getAllUnits(){
+//            return board.getAllUnits();//return all units on the board
+//        }
+//
+//
+//
+//
+//
+//
+//        //debugging return string
+//        @Override
+//        public String toString(){
+//            return "GameState: "
+//                    + "Turn NUmber: " + turnNumber
+//                    + "Active Player: " + activePlayer
+//                    + "Game Phase: " + currentPhase
+//                    + "Human HP: " + humanState.getHealth()
+//                    + "Human Mana: " + humanState.getMana()
+//                    + "AI HP: " + aiState.getHealth()
+//                    + "AI Mana: " + aiState.getMana()
+//                    + gameOver + winner;
+//        }
+//    }
+//
